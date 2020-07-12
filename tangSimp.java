d0dd1ea1f989da52b4839130c44e7e0b7707909e
fighterkirby3 <57018604+fighterkirby3@users.noreply.github.com>
2020-07-12 12:33:30 -0400

    Add files via upload

---
+++ tangSimp.java
@@ -0,0 +1,7 @@
+public class TangSimpTesting {
+
+    public static void main(String[] args) {
+        System.out.println("jason tang is an excellent programmer. he is a great man and I would love to collab with him.");
+        System.out.println("Hi");
+    }
+}
